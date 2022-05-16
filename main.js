d556d8bc6b21ff7c28c093aea7b78e84c8b94d58
Dawam <100006614+AzmiDawami24@users.noreply.github.com>
2022-05-16 12:04:50 +0700

    Salty Spytoon Project

---
+++ main.js
@@ -0,0 +1,11 @@
+alert("Selamat Datang Di Salty Spytoon, Seberapa Tangguh Dirimu?")
+
+var nama = prompt("Isi nama, biar kenal aja :)");
+alert("Halo, " + nama);
+
+var greget = prompt("Ceritakan Pengalaman Gregetmu!")
+alert("Silahkan Masuk")
+
+
+
+
